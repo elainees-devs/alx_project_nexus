c7198a9a5039234344b3f72238a23f346717ab6f
elainees-devs <elainees.devs@gmail.com>
2025-09-16 14:55:59 +0300

    Add SignupView with form handling and success message

--- jobsboard/users/views.py
+++ jobsboard/users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import SignupForm
+from django.contrib import messages
 
-# Create your views here.
+# Signup view
+class SignupView(View):
+    def get(self, request):
+        return render(request, 'template/signup.html', {'form': 'form'})
+
+    def post(self, request):
+        # Handle user signup logic here
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, 'Account created successfully. Please log in.')
+            return redirect('login')
+        return render(request, 'template/signup.html', {'form': form})
+       
+        
+
+# Login view
+
+# Logout view
+
+# Profile view
+
