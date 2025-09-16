3a1888f64d2e272c47f8efdfb0084d5cd427e8cc
elainees-devs <elainees.devs@gmail.com>
2025-09-16 14:10:10 +0300

    add custom User model with roles and automatic group assignment

--- jobsboard/users/models.py
+++ jobsboard/users/models.py
@@ -1,7 +1,50 @@
+from django.contrib.auth.models import Group, AbstractUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 
-# Custom user model extending AbstractUser to include roles
+# Signal to assign user to a group based on their role
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def assign_group_based_on_role(sender, instance, created, **kwargs):
+    if created:
+        if instance.role == instance.ROLE_SEEKER:
+            group, _ = Group.objects.get_or_create(name='Job Seekers')
+            instance.groups.add(group)
+        elif instance.role == instance.ROLE_RECRUITER:
+            group, _ = Group.objects.get_or_create(name='Recruiters')
+            instance.groups.add(group)
+        elif instance.role == instance.ROLE_ADMIN:
+            group, _ = Group.objects.get_or_create(name='Administrators')
+            instance.groups.add(group)
+        # No need to call instance.save() here, adding to ManyToManyField is enough
+
+# Custom user manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+    
+
+# Custom user model
 class User(AbstractUser):
     ROLE_SEEKER = 'SEEKER'
     ROLE_RECRUITER = 'RECRUITER'
@@ -19,14 +62,23 @@ class User(AbstractUser):
         default=ROLE_SEEKER,
     )
 
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
+    
     # Role check methods
+    @property
     def is_seeker(self): 
         return self.role == self.ROLE_SEEKER
 
+    @property
     def is_recruiter(self):
         return self.role == self.ROLE_RECRUITER
 
+    @property
     def is_admin(self):
         return self.role == self.ROLE_ADMIN
-
-   
