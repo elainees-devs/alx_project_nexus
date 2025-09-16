c016fb4dec586a9bb6a98b3802746e1c994feab6
elainees-devs <elainees.devs@gmail.com>
2025-09-16 15:01:26 +0300

    Add LoginView with authentication, success, and error messages

--- jobsboard/users/views.py
+++ jobsboard/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Signup view
 class SignupView(View):
@@ -9,7 +11,7 @@ class SignupView(View):
         return render(request, 'template/signup.html', {'form': 'form'})
 
     def post(self, request):
-        # Handle user signup logic here
+        # Handle user signup logic
         form=SignupForm(request.POST)
         if form.is_valid():
             user=form.save(commit=False)
@@ -20,8 +22,25 @@ class SignupView(View):
         return render(request, 'template/signup.html', {'form': form})
        
         
-
 # Login view
+class LoginView(View):
+    def get(self, request):
+        form=LoginForm()
+        return render(request, 'template/login.html', {'form': form})
+
+    def post(self, request):
+        # Handle user login logic
+        form=LoginForm(request.POST)
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Logged in successfully.')
+            return redirect('template/profile')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return render(request, 'template/login.html', {'form': form})
 
 # Logout view
 
