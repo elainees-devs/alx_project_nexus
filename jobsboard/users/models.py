79f9ff141d6dd3148f4c3023d4c5768a8b475d06
elainees-devs <elainees.devs@gmail.com>
2025-09-16 17:03:06 +0300

    Add Profile model and signal to automatically create/update Profile when User is created

--- jobsboard/users/models.py
+++ jobsboard/users/models.py
@@ -112,3 +112,23 @@ def assign_group_and_permissions(sender, instance, created, **kwargs):
 
     # Add user to the group
     instance.groups.add(group)
+
+# Profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=255, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+
+# Signal to automatically create/update Profile when User is created
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
