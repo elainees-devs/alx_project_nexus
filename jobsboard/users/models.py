7487ce40bcfc2437e2f1154b50a4bc5ad4a35e8e
elainees-devs <elainees.devs@gmail.com>
2025-09-16 14:41:15 +0300

    Add role-based permissions and group assignment with logging

--- jobsboard/users/models.py
+++ jobsboard/users/models.py
@@ -1,22 +1,11 @@
-from django.contrib.auth.models import Group, AbstractUser, BaseUserManager
+from django.contrib.auth.models import Group, AbstractUser, BaseUserManager, Permission
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+import logging
 
-# Signal to assign user to a group based on their role
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def assign_group_based_on_role(sender, instance, created, **kwargs):
-    if created:
-        if instance.role == instance.ROLE_SEEKER:
-            group, _ = Group.objects.get_or_create(name='Job Seekers')
-            instance.groups.add(group)
-        elif instance.role == instance.ROLE_RECRUITER:
-            group, _ = Group.objects.get_or_create(name='Recruiters')
-            instance.groups.add(group)
-        elif instance.role == instance.ROLE_ADMIN:
-            group, _ = Group.objects.get_or_create(name='Administrators')
-            instance.groups.add(group)
+logger = logging.getLogger(__name__)
 
 # Custom user manager
 class CustomUserManager(BaseUserManager):
@@ -42,6 +31,7 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(username, email, password, **extra_fields)
 
+
 # Custom user model
 class User(AbstractUser):
     ROLE_SEEKER = 'SEEKER'
@@ -54,12 +44,13 @@ class User(AbstractUser):
         (ROLE_ADMIN, 'Administrator'),
     ]
 
-    role = models.CharField(
-        max_length=15,
-        choices=ROLE_CHOICES,
-        default=ROLE_SEEKER,
-    )
+    ROLE_PERMISSIONS = {
+        'Job Seeker': ['view_user'],
+        'Recruiter': ['add_user', 'view_user', 'change_user_role'],
+        'Administrator': ['add_user', 'view_user', 'change_user_role', 'deactivate_user'],
+    }
 
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES, default=ROLE_SEEKER)
     middle_name = models.CharField(max_length=30, blank=True, null=True)
     last_login_ip = models.GenericIPAddressField(null=True, blank=True)
 
@@ -68,12 +59,20 @@ class User(AbstractUser):
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
 
+    class Meta:
+        permissions = [
+            ('add_user', 'Can Add User'),
+            ('view_user', 'Can View User'),
+            ('change_user_role', 'Can Change User Role'),
+            ('deactivate_user', 'Can Deactivate User'),
+        ]
+
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"
 
-    # Role check methods
+    # Role check properties
     @property
-    def is_seeker(self): 
+    def is_seeker(self):
         return self.role == self.ROLE_SEEKER
 
     @property
@@ -83,3 +82,33 @@ class User(AbstractUser):
     @property
     def is_admin(self):
         return self.role == self.ROLE_ADMIN
+
+
+# Signal to assign group and permissions
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def assign_group_and_permissions(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    # Determine group name based on role
+    if instance.role == instance.ROLE_SEEKER:
+        group_name = 'Job Seekers'
+    elif instance.role == instance.ROLE_RECRUITER:
+        group_name = 'Recruiters'
+    elif instance.role == instance.ROLE_ADMIN:
+        group_name = 'Administrators'
+    else:
+        return
+
+    # Get or create the group
+    group, _ = Group.objects.get_or_create(name=group_name)
+
+    # Assign permissions to the group (only if missing)
+    role_name = dict(instance.ROLE_CHOICES).get(instance.role)
+    if role_name:
+        perm_codenames = instance.ROLE_PERMISSIONS.get(role_name, [])
+        perms = Permission.objects.filter(codename__in=perm_codenames)
+        group.permissions.add(*perms)
+
+    # Add user to the group
+    instance.groups.add(group)
