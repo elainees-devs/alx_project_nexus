145d83ab049ae0bca599b8ae2e6ef3a61c099887
elainees-devs <elainees.devs@gmail.com>
2025-09-17 17:55:58 +0300

    Add admin registration with decorators for Job, Skill, and JobSkill models

--- jobsboard/jobs/admin.py
+++ jobsboard/jobs/admin.py
@@ -1,3 +1,21 @@
+# jobsboard/jobs/admin.py
 from django.contrib import admin
+from .models import Job, Skill, JobSkill
 
-# Register your models here.
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'company', 'employment_type', 'work_location_type', 'experience_level', 'status', 'posted_date', 'closing_date')
+    list_filter = ('employment_type', 'work_location_type', 'experience_level', 'status', 'company')
+    search_fields = ('title', 'description', 'location', 'company__name')
+    date_hierarchy = 'posted_date'
+    filter_horizontal = ('skills',)
+
+@admin.register(JobSkill)
+class JobSkillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'job', 'skill')
+    search_fields = ('job__title', 'skill__name')
