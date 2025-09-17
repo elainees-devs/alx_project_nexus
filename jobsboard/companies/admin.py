636941254d1b9569b79df6fc66d429c21fcb04b0
elainees-devs <elainees.devs@gmail.com>
2025-09-17 17:55:16 +0300

    Refactor companies admin.py to use decorators for registering Industry and Company with custom admin

--- jobsboard/companies/admin.py
+++ jobsboard/companies/admin.py
@@ -1,19 +1,13 @@
 from django.contrib import admin
 from .models import Industry, Company
 
-# Customize how the Industry model appears in admin  interface
+@admin.register(Industry)
 class IndustryAdmin(admin.ModelAdmin):
-    list_display=("id", "name") # columns to display
-    search_fields=["name"]
+    list_display = ("id", "name")  # columns to display
+    search_fields = ["name"]        # searchable fields
 
-
+@admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "description","website", "location", "industry")
-    search_fields=["name", "location", "industry__name"]
+    list_display = ("id", "name", "description", "website", "location", "industry")
+    search_fields = ["name", "location", "industry__name"]
     list_filter = ("industry",)
-   
-
-# Register the model with its custom admin configuration
-admin.site.register(Industry,IndustryAdmin)
-admin.site.register(Company, CompanyAdmin)
-
