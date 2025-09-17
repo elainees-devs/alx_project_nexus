c3699c58105dc408782a98d76602d0f49b27b723
elainees-devs <elainees.devs@gmail.com>
2025-09-17 17:42:32 +0300

    Add Job and Skill models with enums, relationships, and JobSkill through table

--- jobsboard/jobs/models.py
+++ jobsboard/jobs/models.py
@@ -1,3 +1,83 @@
+# jobsboard/jobs/models.py
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Skill(models.Model):
+    name=models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plural='Skills'
+
+    def __str__(self):
+        return self.name
+ 
+
+class Job(models.Model):
+       # Enum choices
+    EMPLOYMENT_TYPE_CHOICES = [
+        ("full_time", "Full Time"),
+        ("part_time", "Part Time"),
+        ("contract", "Contract"),
+        ("internship", "Internship"),
+        ("temporary", "Temporary"),
+    ]
+
+
+    WORK_LOCATION_TYPE_CHOICES = [
+        ("onsite", "Onsite"),
+        ("remote", "Remote"),
+        ("hybrid", "Hybrid"),
+    ]
+
+    EXPERIENCE_LEVEL_CHOICES = [
+        ("entry", "Entry"),
+        ("mid", "Mid"),
+        ("senior", "Senior"),
+        ("lead", "Lead"),
+    ]
+
+    JOB_STATUS_CHOICES = [
+        ("open", "Open"),
+        ("closed", "Closed"),
+        ("draft", "Draft"),
+    ]
+
+    title=models.CharField(max_length=255)
+    description=models.TextField()
+    location=models.CharField(max_length=255)
+    employment_type=models.CharField(max_length=20, choices=EMPLOYMENT_TYPE_CHOICES)
+    work_location_type=models.CharField(max_length=20, choices=WORK_LOCATION_TYPE_CHOICES)
+    experience_level=models.CharField(max_length=20, choices=EXPERIENCE_LEVEL_CHOICES)
+    salary_min=models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True )
+    salary_max=models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True )
+    posted_date=models.DateTimeField(auto_now_add=True)
+    closing_date=models.DateTimeField(blank=True, null=True)
+    status=models.CharField(max_length=20, choices=JOB_STATUS_CHOICES)
+    company = models.ForeignKey(
+        'companies.Company', 
+        on_delete=models.CASCADE,
+        related_name="jobs"
+    )
+    skills=models.ManyToManyField("Skill", related_name="jobs", through="JobSkill")
+    created_by = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True,
+    related_name="created_jobs"
+)
+
+
+    def __str__(self):
+        return self.title
+    
+
+class JobSkill(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('job', 'skill')
+
+    def __str__(self):
+        return f"{self.job.title} - {self.skill.name}"
