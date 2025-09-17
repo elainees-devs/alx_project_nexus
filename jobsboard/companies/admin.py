7f3b1fb8b3541ebc83519d7d190eabb39f70b021
elainees-devs <elainees.devs@gmail.com>
2025-09-17 09:53:31 +0300

    add custom admin for Industry and Company models

--- jobsboard/companies/admin.py
+++ jobsboard/companies/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Industry, Company
 
-# Register your models here.
+# Customize how the Industry model appears in admin  interface
+class IndustryAdmin(admin.ModelAdmin):
+    list_display=("id", "name") # columns to display
+    search_fields=["name"]
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description","website", "location", "industry")
+    search_fields=["name", "location", "industry__name"]
+    list_filter = ("industry",)
+   
+
+# Register the model with its custom admin configuration
+admin.site.register(Industry,IndustryAdmin)
+admin.site.register(Company, CompanyAdmin)
+
