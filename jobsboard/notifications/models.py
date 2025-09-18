c609a5033f316ad2f591bc629c46183d2123bd0f
elainees-devs <elainees.devs@gmail.com>
2025-09-18 15:07:07 +0300

    Add Notification model with user, title, message, link, type, is_read, and created_at fields

--- jobsboard/notifications/models.py
+++ jobsboard/notifications/models.py
@@ -1,3 +1,30 @@
+#jobsboard/notifications/models.py
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Notification(models.Model):
+    NOTIFICATION_TYPE=[
+        ('info', 'Info'),
+        ('warning', 'Warning'),
+        ('success', 'Success'),
+        ('error', 'Error')
+    ]
+    user=models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
+    title=models.CharField(max_length=255)
+    messages=models.TextField()
+    link=models.CharField(max_length=255, blank=True, null=True)
+    is_read=models.BooleanField(default=False)
+    type=models.CharField(
+        max_length=20,
+        choices=NOTIFICATION_TYPE,
+        default='info'
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.title} ({'Read' if self.is_read else 'Unread'})"
+
