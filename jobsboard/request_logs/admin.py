50a2bf9b9f757db09d6fbb9d6403060397b24e45
elainees-devs <elainees.devs@gmail.com>
2025-09-19 11:59:16 +0300

    Add admin for request_logs to view all request logs

--- jobsboard/request_logs/admin.py
+++ jobsboard/request_logs/admin.py
@@ -1,3 +1,10 @@
+# jobsboard/request_logs/admin.py
 from django.contrib import admin
+from .models import RequestLog
 
-# Register your models here.
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "ip_address", "endpoint", "method", "status_code", "timestamp")
+    list_filter = ("method", "status_code", "timestamp")
+    search_fields = ("user__username", "ip_address", "endpoint")
+    ordering = ("-timestamp",)
