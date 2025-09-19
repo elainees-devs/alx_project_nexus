c76a7031d47c18e29cdb059efe5b06205bbf8cb5
elainees-devs <elainees.devs@gmail.com>
2025-09-19 11:29:35 +0300

    Add rate_limit URLs to API routing

--- jobsboard/api/urls.py
+++ jobsboard/api/urls.py
@@ -35,6 +35,7 @@ urlpatterns = [
     path('api/', include('applications.urls')),
     path('api/', include('payments.urls')),
     path('api/', include('notifications.urls')),
+    path('api/', include('rate_limit.urls')),
     re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
