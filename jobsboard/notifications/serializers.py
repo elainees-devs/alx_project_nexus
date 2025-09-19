4b921e1cbbe87c039eaaec41fe12e26d6bd3ab08
elainees-devs <elainees.devs@gmail.com>
2025-09-19 22:20:39 +0300

    Update notification serializers to include link and type fields in create serializer

--- jobsboard/notifications/serializers.py
+++ jobsboard/notifications/serializers.py
@@ -2,25 +2,27 @@
 from rest_framework import serializers
 from .models import Notification
 
+
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         fields = [
-            'id',
-            'user',
-            'title',
-            'message',
-            'link',
-            'type',
-            'is_read',
-            'created_at',
+            "id",
+            "user",
+            "title",
+            "message",
+            "link",
+            "type",
+            "is_read",
+            "created_at",
         ]
-        read_only_fields = ['id', 'user', 'created_at']
+        read_only_fields = ["id", "user", "created_at"]
+
 
 class NotificationCreateSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Notification
-        fields = ["id", "title", "message"]
-
+        fields = ["id", "title", "message", "link", "type"]
+        read_only_fields = ["id"]
