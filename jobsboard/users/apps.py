eb1cad896e6390ca9002eb36f372fdfca223c4cf
elainees-devs <elainees.devs@gmail.com>
2025-09-27 01:20:05 +0300

    apps(users): add post_migrate signal to reset users_profile sequence for correct ID continuity

--- jobsboard/users/apps.py
+++ jobsboard/users/apps.py
@@ -1,6 +1,26 @@
+# users/apps.py
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+from django.db import connection
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
+
+    def ready(self):
+        post_migrate.connect(reset_profile_sequence, sender=self)
+
+
+def reset_profile_sequence(sender, **kwargs):
+    """
+    Reset sequence for users_profile so new rows get the correct ID.
+    """
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT setval(
+              pg_get_serial_sequence('"users_profile"', 'id'),
+              COALESCE((SELECT MAX(id) FROM "users_profile"), 1),
+              TRUE
+            );
+        """)
